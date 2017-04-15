be717b60d08bf8a5380b71753576d9d209865e22
Siva <Siva.N.Polam@kp.org>
2017-04-15 18:04:46 +0530

    Initial commit

---
+++ app/src/main/java/com/techplicit/mycarnival/TestActivity.java
@@ -0,0 +1,18 @@
+package com.techplicit.mycarnival;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by pnaganjane001 on 4/15/17.
+ */
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_activity);
+    }
+}
